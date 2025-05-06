7ac2cb376bef4bcdb1d1dc0c7e6cf7a3fbf25541
Egor Kachanouski <kegor2323@gmail.com>
2025-05-07 00:19:52 +0200

    Front-end with React.ts + vite + tailwind initialized

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600">
+        <h1 className="text-4xl font-bold text-white">Hello Tailwind + React + Vite + TS!</h1>
+      </div>
+  );
+}
+
+export default App
