72e57a7879c1e819725bf8bd4a59dd2adc1f3b50
Egor Kachanouski <kegor2323@gmail.com>
2025-05-06 23:22:04 +0200

    Controller added

---
+++ BackEnd/Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using BackEnd.DTOs;
+using BackEnd.Services.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ILLMService _LLMService;
+
+        public MessagesController(ILLMService LLMService)
+        {
+            _LLMService = LLMService;
+        }
+
+        [HttpPost("chat-history")]
+        public async Task<IActionResult> SendChatHistory([FromBody] ChatHistoryDto chatHistoryDto) 
+        {
+            try
+            {
+                var response = await _LLMService.SendMessageAsync(chatHistoryDto);
+                return Ok(response);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(new { Message = "Apologies, something went wrong", ErrorMessage = ex.Message });
+            }
+        }
+
+    }
+}
